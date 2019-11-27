2b6b1a60b44b953423559298cb07f76ababb5430
AnonymerNiklasistanonym <niklas.mikeler@gmail.com>
2019-11-27 04:41:15 +0100

    Update scripts to work on linux (and update fonts)

--- images/favicon/createFaviconFiles.sh
+++ images/favicon/createFaviconFiles.sh
@@ -19,8 +19,16 @@ array=( 32 512 )
 for i in "${array[@]}"
 do
 	# Export each size as a `png` favicon from `favicon.svg`
-	inkscape ".\favicon.svg" --export-png="favicon_"$i".png" --export-width=$i --export-height=$i --without-gui
+	inkscape "favicon.svg" --export-png="favicon_"$i".png" --export-width=$i --export-height=$i --without-gui
 done
 
 # ICO export:
-magick convert favicon_512.png -define icon:auto-resize=16,32,48,64,96,128,256 favicon.ico
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+	convert favicon_512.png -define icon:auto-resize=16,32,48,64,96,128,256 favicon.ico
+elif [[ "$OSTYPE" == "msys" ]]; then
+	magick convert favicon_512.png -define icon:auto-resize=16,32,48,64,96,128,256 favicon.ico
+else
+	echo "Platform not supported! ($OSTYPE)" >&2
+	exit 1
+fi
+
